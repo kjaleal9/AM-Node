a7611484681b05f320299e9f686a589cf3d19bcb
Kris Leal <kjaleal9@gmail.com>
2020-07-30 13:26:47 -0500

    Refactored code to using functions calls and callbacks

--- WeatherApp/uitls/geocode.js
+++ WeatherApp/uitls/geocode.js
@@ -20,4 +20,30 @@ const geocode = (address, MPUrl, MPapiKey, callback) => {
     });
 };
 
-module.exports = geocode;
+const forecast = (address, WSUrl, WSapiKey, units = 'f', callback) => {
+    const weatherRequest = `${WSUrl}?access_key=${WSapiKey}&query=${address}&units=${units}`;
+
+    request({ url: weatherRequest, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services', undefined);
+        } else if (response.body === 0) {
+            callback('Try again with a different search', undefined);
+        } else {
+            const {
+                temperature,
+                feelslike,
+                weather_descriptions,
+            } = response.body.current;
+            callback(undefined, {
+                temperature: temperature,
+                feelslike: feelslike,
+                weather_descriptions: weather_descriptions[0],
+            });
+        }
+    });
+};
+
+module.exports = {
+    geocode: geocode,
+    forecast: forecast,
+};
