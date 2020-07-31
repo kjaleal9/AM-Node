df0d76c3196684fe9b69bd429b4ae6220810f0ae
Kris Leal <kjaleal9@gmail.com>
2020-07-30 19:37:36 -0500

    Added handlebars module and views and partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,41 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
-const publicDirectory = path.join(__dirname, '../public');
 
+// Define paths for Express config
+const publicDirectory = path.join(__dirname, '../public');
+const viewsDirectory = path.join(__dirname, '../templates/views');
+const partialsDirectory = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsDirectory)
+hbs.registerPartials(partialsDirectory)
+
+// Setup static directory to use
 app.use(express.static(publicDirectory));
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Kris Leal'
+    })
+})
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Kris Leal'
+    })
+})
+app.get('/help',(req,res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Kris Leal',
+        helpText: 'This is some helpful text'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send('Your weather');
